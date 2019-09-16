870d31e61490ab956bbfeee3d6ca573513610c51
Shivam <jalotrashivam9@gmail.com>
2019-09-16 23:39:16 +0530

    Top level python script to run all the function written in uwimg.py

---
+++ tryit2.py
@@ -0,0 +1,25 @@
+
+
+# Python module to test all the written functions
+
+from uwimg import *
+import os
+import sys
+
+directory_name = "Tryit2Photos"
+
+def create_photos_directory():    
+    try:
+        os.mkdir(path = os.path.join(os.getcwd(), directory_name), mode = 777 )
+    except FileExistsError:
+        pass
+
+def nn_interpolate_verification():
+    im = load_image("data/dogsmall.jpg")
+    a = nn_resize(im, im.w*4, im.h*4)
+    save_image(a, directory_name + '/' "dog4x-nn")
+
+
+if __name__ == "__main__":
+    create_photos_directory()
+    nn_interpolate_verification()
