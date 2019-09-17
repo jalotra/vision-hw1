c892dbda338b15bc70167900fbe1df6dbee26814
Shivam <jalotrashivam9@gmail.com>
2019-09-17 19:03:35 +0530

    Added bilinear interpolation function

--- tryit2.py
+++ tryit2.py
@@ -17,9 +17,15 @@ def create_photos_directory():
 def nn_interpolate_verification():
     im = load_image("data/dogsmall.jpg")
     a = nn_resize(im, im.w*4, im.h*4)
-    save_image(a, directory_name + '/' "dog4x-nn")
+    save_image(a, directory_name + '/' + "dog4x-nn")
+
+def bilinear_interpolate_verification():
+    im = load_image("data/dogsmall.jpg")
+    a = bilinear_resize(im, im.w*4, im.h*4)
+    save_image(a, directory_name + '/' + 'bilinear_dog_4x' )
 
 
 if __name__ == "__main__":
     create_photos_directory()
     nn_interpolate_verification()
+    bilinear_interpolate_verification()
