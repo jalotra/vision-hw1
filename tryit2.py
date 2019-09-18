48520885840eeb586f2d550a6c5600f78ed38c88
Shivam <jalotrashivam9@gmail.com>
2019-09-18 19:23:25 +0530

    Custom python functions to test convolution

--- tryit2.py
+++ tryit2.py
@@ -6,7 +6,7 @@ from uwimg import *
 import os
 import sys
 
-directory_name = "Tryit2Photos"
+directory_name = "Tryit2Photos/"
 
 def create_photos_directory():    
     try:
@@ -15,17 +15,24 @@ def create_photos_directory():
         pass
 
 def nn_interpolate_verification():
-    im = load_image("data/dogsmall.jpg")
-    a = nn_resize(im, im.w*4, im.h*4)
-    save_image(a, directory_name + '/' + "dog4x-nn")
+    im = load_image("data/ron.png")
+    a = nn_resize(im, im.w/2, im.h/2)
+    save_image(a, directory_name  + "dog4x-nn")
 
 def bilinear_interpolate_verification():
     im = load_image("data/dogsmall.jpg")
-    a = bilinear_resize(im, im.w*4, im.h*4)
-    save_image(a, directory_name + '/' + 'bilinear_dog_4x' )
+    a = bilinear_resize(im, im.w*8, im.h*8)
+    save_image(a, directory_name + 'bilinear_dog_4x' )
+
+def test_convolution():
+    im = load_image("data/dog.jpg")
+    f = make_box_filter(3)
+    blur = convolve_image(im, f, 0)
+    save_image(blur, directory_name + "box_filter")
 
 
 if __name__ == "__main__":
     create_photos_directory()
-    nn_interpolate_verification()
-    bilinear_interpolate_verification()
+    #  nn_interpolate_verification()
+    # bilinear_interpolate_verification()
+    test_convolution()
