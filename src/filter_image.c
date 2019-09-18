4b07160b0e42b22a63eec6446797b59809083829
Shivam <jalotrashivam9@gmail.com>
2019-09-18 20:00:38 +0530

    Implemented highpass, sharpen, emboss filter

--- src/filter_image.c
+++ src/filter_image.c
@@ -51,24 +51,6 @@ image make_box_filter(int w)
 
 }
 
-// float box_filter_result(image im, image filter, int row, int column, int channel, int preserve)
-// {
-//     int f_c;
-//      if(filter.c == 1) f_c = 0 ;
-//      else f_c = channel;
-
-//     float sum = 0 ;
-//     for(int i = -filter.w/2; i <=(filter.w/2); i++)
-//     {
-//         for(int k = -filter.h/2; k <=(filter.h/2); k++)
-//         {   
-//             sum += get_pixel(filter, filter.w /2 +i , filter.h/2 + k, f_c) * get_pixel(im, column+i , row + k , channel); 
-//         }
-//     }
-
-//     return sum;
-//     // printf("%f ",sum);
-// }
 
 float box_filter_result(image im, image filter, int row, int column, int channel, int preserve)
 {
@@ -184,19 +166,75 @@ image convolve_image(image im, image filter, int preserve)
 image make_highpass_filter()
 {
     // TODO
-    return make_image(1,1,1);
+
+    // highpass filter involves the use pf sobel filter
+    image highpass_filter = make_image(3,3,1);
+    // Fill the zeoes
+    set_pixel(highpass_filter, 0, 0, 0, 0);
+    set_pixel(highpass_filter, 2, 0, 0, 0);
+    set_pixel(highpass_filter, 0, 2, 0, 0);
+    set_pixel(highpass_filter, 2, 2, 0, 0);
+
+    // Fill -1s
+    set_pixel(highpass_filter, 1, 0, 0, -1);
+    set_pixel(highpass_filter, 0, 1,0, -1);
+    set_pixel(highpass_filter, 2, 1, 0,-1);
+    set_pixel(highpass_filter, 1, 2, 0,-1);
+
+    // fill 4
+    set_pixel(highpass_filter, 1, 1,0, 4);
+
+
+
+    return highpass_filter;
 }
 
 image make_sharpen_filter()
 {
     // TODO
-    return make_image(1,1,1);
+     image sharpen_filter = make_image(3,3,1);
+    // Fill the zeoes
+    set_pixel(sharpen_filter, 0, 0, 0, 0);
+    set_pixel(sharpen_filter, 2, 0, 0, 0);
+    set_pixel(sharpen_filter, 0, 2, 0, 0);
+    set_pixel(sharpen_filter, 2, 2, 0, 0);
+
+    // Fill -1s
+    set_pixel(sharpen_filter, 1, 0, 0, -1);
+    set_pixel(sharpen_filter, 0, 1,0, -1);
+    set_pixel(sharpen_filter, 2, 1, 0,-1);
+    set_pixel(sharpen_filter, 1, 2, 0,-1);
+
+    // fill 4
+    set_pixel(sharpen_filter, 1, 1,0, 5);
+
+
+
+    return sharpen_filter;
 }
 
 image make_emboss_filter()
 {
     // TODO
-    return make_image(1,1,1);
+    image emboss_filter = make_image(3,3,1);
+    // Fill the zeoes
+    set_pixel(emboss_filter, 0, 0, 0, -2);
+    set_pixel(emboss_filter, 2, 0, 0, 0);
+    set_pixel(emboss_filter, 0, 2, 0, 0);
+    set_pixel(emboss_filter, 2, 2, 0, 2);
+
+    // Fill -1s
+    set_pixel(emboss_filter, 1, 0, 0, -1);
+    set_pixel(emboss_filter, 0, 1,0, -1);
+    set_pixel(emboss_filter, 2, 1, 0,1);
+    set_pixel(emboss_filter, 1, 2, 0,-1);
+
+    // fill 4
+    set_pixel(emboss_filter, 1, 1,0, 1);
+
+
+
+    return emboss_filter;
 }
 
 // Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we not? Why?
--- tryit2.py
+++ tryit2.py
@@ -26,13 +26,42 @@ def bilinear_interpolate_verification():
 
 def test_convolution():
     im = load_image("data/dog.jpg")
-    f = make_box_filter(3)
-    blur = convolve_image(im, f, 0)
+    f = make_box_filter(7)
+    blur = convolve_image(im, f, 1)
     save_image(blur, directory_name + "box_filter")
+
+def shrink_after_convolution():
+    im = load_image("data/dog.jpg")
+    f = make_box_filter(7)
+    blur = convolve_image(im, f, 1)
+    thumb = nn_resize(blur, blur.w//7, blur.h//7)
+    save_image(thumb, directory_name+"dogthumb")
+
+def test_highpass_filter():
+    im = load_image("data/dog.jpg")
+    f = make_highpass_filter()
+    blur = convolve_image(im, f, 1)
+    save_image(blur, directory_name + "test_highpass_filter")
+
+def test_sharpen_filter():
+    im = load_image("data/dog.jpg")
+    f = make_sharpen_filter()
+    blur = convolve_image(im, f, 1)
+    save_image(blur, directory_name + "test_sharpen_filter")
+
+def test_emboss_filter():
+    im = load_image("data/dog.jpg")
+    f = make_emboss_filter()
+    blur = convolve_image(im, f, 1)
+    save_image(blur, directory_name + "test_emboss_filter")
 
 
 if __name__ == "__main__":
     create_photos_directory()
     #  nn_interpolate_verification()
     # bilinear_interpolate_verification()
-    test_convolution()
+    # test_convolution()
+    # shrink_after_convolution()
+    test_highpass_filter()
+    test_sharpen_filter()
+    test_emboss_filter()
